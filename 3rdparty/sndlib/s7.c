5dc0f68306d908a8bd72509881ba217664573a7d
lerch <lerch@zplane.de>
2016-01-20 11:47:50 -0500

    - quick hack to make it compile under MSVC2015 (but break s7!)

--- 3rdparty/sndlib/s7.c
+++ 3rdparty/sndlib/s7.c
@@ -7148,8 +7148,9 @@ double cbrt(double x)
   return(-pow(-x, 1.0 / 3.0));
 }
 
-static bool is_inf(s7_Double x) {return((x == x) && (is_NaN(x - x)));}
-
+//static bool is_inf(s7_Double x) { return((x == x) && (is_NaN(x - x))); }
+//static bool is_inf(double x) { return true; }
+#define is_inf(a) (true)
 #endif
 
 
